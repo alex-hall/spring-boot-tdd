6da1781e62acbd753963d1877eac2bd3a72498e2
Alex Hall <me@jalexhall.com>
2017-11-21 11:20:38 -0500

    But we’ve created two new classes that don’t have tests!
    This begs the question: What code should be tested? My answer? Any code
    That you rely on in production. Period.
    
    Knowing when to Unit test is probably the most difficult part of TDD in 
    my opinion. A more statically typed language like java is going to have
    More boilerplate than a dynamic language. Typically in Java you don’t 
    HAVE to unit test things like Getters/Setters or any generated
    Code that DOESN”T have business logic in it.

--- src/main/java/com/alex/user/User.java
+++ src/main/java/com/alex/user/User.java
@@ -11,4 +11,38 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public void setId(Integer id){ this.id = id;}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
