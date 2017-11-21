ea2a6f610cca579f71897db7ddc02640584e4ecc
Alex Hall <me@jalexhall.com>
2017-11-21 11:01:32 -0500

    Note our controller isn’t actually doing anything yet, that’s okay.
    The error we should be getting now is that we’re basically not returning
    JSON. 
    
    The strict TDD thing to do here (Remember: Red-Green-Refactor) 
    would be to have our controller Return a static JSON string, then in 
    the Refactor step, refactor our code to actually call a repository.
    
    For the sake of brevity, i’m going to combine the two steps.

--- src/main/java/com/alex/user/UserController.java
+++ src/main/java/com/alex/user/UserController.java
@@ -1,14 +1,20 @@
 package com.alex.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping(value="/user/{id}", method= RequestMethod.GET)
     @ResponseBody
-    public void getUser(@PathVariable Integer id){
+    public User getUser(@PathVariable Integer id){
+        return userRepository.findOne(id);
+    }
 
     }
 }
